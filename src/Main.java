4d27cca20747b90a1378333e8c3c26b309278934
Nataliya <nataliyasjmonova@gmail.com>
2023-10-09 17:56:58 +0200

    anagram

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Solution solution = new Solution();
+        String s1 = "anagram";
+        String t1 = "nagaram";
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        boolean result1 = solution.isAnagram(s1, t1);
+        System.out.println(result1);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        String s2 = "rat";
+        String t2 = "car";
+
+        boolean result2 = solution.isAnagram(s2, t2);
+        System.out.println(result2);
     }
 }
