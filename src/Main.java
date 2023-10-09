0f8412721c71f9e8dd9e392173dd2cf702db7aae
Nataliya <nataliyasjmonova@gmail.com>
2023-10-10 00:08:05 +0200

    animals

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Dog dogBobik = new Dog("Бобік");
+        Cat catBarsik = new Cat("Барсік");
+        Dog dogRex = new Dog("Рекс");
+        Dog dogOskar = new Dog("Оскар");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        dogBobik.run(150);
+        dogBobik.swim(5);
+        dogRex.run(600);
+        catBarsik.run(199);
+        catBarsik.swim(3);
+
+        System.out.println("Кількість собак: " + Dog.getTotalCount());
+        System.out.println("Кількість котів: " + Cat.getTotalCount());
+        System.out.println("Кількість всіх тварин: " + Animal.getTotalAnimals());
     }
 }
